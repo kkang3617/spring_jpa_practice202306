074660966c28f9af93f8292c430900a23aea7614
kkang3617 <rkdgus3617@naver.com>
2023-06-19 18:13:35 +0900

    DAY82 JPA 연관관계 매핑(양방향), FETCH JOIN(N+1 예제), 종합연습예제(페이징)

---
+++ src/test/java/com/spring/jpa/chap05_practice/repository/PostRepositoryTest.java
@@ -0,0 +1,32 @@
+package com.spring.jpa.chap05_practice.repository;
+
+import com.spring.jpa.chap05_practice.entity.Post;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class PostRepositoryTest {
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    @DisplayName("bulk insert")
+    void bulkInsert() {
+        for ( int i=1; i<=378; i++) {
+            postRepository.save(
+                    Post.builder()
+                            .title("하하하제목" + i)
+                            .content("하하하내용" + i)
+                            .writer("하하하작성자" + i)
+                            .build()
+            );
+        }
+    }
+
+}
